586fed9b28292386d7a81fdf7277c295aa4b3e65
plana93 <mirco.pl.93@gmail.com>
2023-12-23 23:59:56 +0100

    add more answer in the python section basics_1

---
+++ Python Interview Preparation/code/basics_1.py
@@ -0,0 +1,26 @@
+# Example to Explain Shallow and Deep Copy
+
+import copy
+
+# Original List with nested list
+original_list = [1, [2, 3], [4, 5]]
+
+# Shallow Copy
+shallow_copy = copy.copy(original_list)
+
+# Deep Copy
+deep_copy = copy.deepcopy(original_list)
+
+# Modify the original list
+original_list[1][0] = 'X'
+
+# Output
+print("Original List:", original_list)
+print("Shallow Copy:", shallow_copy)
+print("Deep Copy:", deep_copy)
+
+############################################################
+############################################################
+
+
+
